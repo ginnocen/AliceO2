df1761caf33ba7f5c789f90afe9620cb428d2183
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-02-14 23:06:50 +0100

    DPL Analysis: introduce index columns
    This is still work in progress, because we do not actually do any binding
    in the processing, yet. It should however compile and still allow people
    to use the old "by hand" mechanism.

--- Framework/Core/include/Framework/ASoA.h
+++ Framework/Core/include/Framework/ASoA.h
@@ -827,6 +827,53 @@ class TableMetadata
   static const o2::framework::expressions::BindingNode _Getter_ { _Label_,     \
                                                                   o2::framework::expressions::selectArrowType<_Type_>() }
 
+/// An index column is a column of indices to elements / of another table named
+/// _Name_##s. The column name will be _Name_##Id and will always be stored in
+/// "f"#_Name__#Id . It will also have two special methods, setCurrent(...)
+/// and getCurrent(...) which allow you to set / retrieve associated table.
+/// It also exposes a getter _Getter_ which allows you to retrieve the pointed
+/// object.
+/// Notice how in order to define an index column, the table it points
+/// to **must** be already declared. This is therefore only
+/// useful to express child -> parent relationships. In case one
+/// needs to go from parent to child, the only way is to either have
+/// a separate "association" with the two indices, or to use the standard
+/// grouping mechanism of AnalysisTask.
+#define DECLARE_SOA_INDEX_COLUMN_FULL(_Name_, _Getter_, _Type_, _Table_)                 \
+  struct _Name_##Id : o2::soa::Column<_Type_, _Name_##Id> {                              \
+    static_assert(std::is_integral_v<_Type_>, "Index type must be integral");            \
+    static constexpr const char* mLabel = "f" #_Name_ "ID";                              \
+    using base = o2::soa::Column<_Type_, _Name_##Id>;                                    \
+    using type = _Type_;                                                                 \
+    using column_t = _Name_##Id;                                                         \
+    using binding_t = _Table_;                                                           \
+    _Name_##Id(arrow::Column const* column)                                              \
+      : o2::soa::Column<_Type_, _Name_##Id>(o2::soa::ColumnIterator<type>(column))       \
+    {                                                                                    \
+    }                                                                                    \
+                                                                                         \
+    _Name_##Id() = default;                                                              \
+    _Name_##Id(_Name_##Id const& other) = default;                                       \
+    _Name_##Id& operator=(_Name_##Id const& other) = default;                            \
+                                                                                         \
+    type const _Getter_##Id() const                                                      \
+    {                                                                                    \
+      return *mColumnIterator;                                                           \
+    }                                                                                    \
+                                                                                         \
+    binding_t::iterator _Getter_() const                                                 \
+    {                                                                                    \
+      constexpr auto getBegin = [](auto* p) { return p->begin(); };                      \
+      return mBinding->begin() + *mColumnIterator;                                       \
+    }                                                                                    \
+    void setCurrent(binding_t* current) { mBinding = current; }                          \
+    binding_t* getCurrent() { return mBinding; }                                         \
+    binding_t* mBinding;                                                                 \
+  };                                                                                     \
+  static const o2::framework::expressions::BindingNode _Getter_##Id { "f" #_Name_ "sID", \
+                                                                      o2::framework::expressions::selectArrowType<_Type_>() }
+
+#define DECLARE_SOA_INDEX_COLUMN(_Name_, _Getter_) DECLARE_SOA_INDEX_COLUMN_FULL(_Name_, _Getter_, int32_t, _Name_##s)
 /// A dynamic column is a column whose values are derived
 /// from those of other real columns. These can be used for
 /// example to provide different coordinate systems (e.g. polar,
