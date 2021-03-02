53a9f12701ece094e388d56e966926b6dbd3af4b
Deepa Thomas <deepa.thomas@cern.ch>
2021-03-01 22:22:09 -0600

    Adding histos of B candidate properties

--- Analysis/Tasks/PWGHF/taskBPlus.cxx
+++ Analysis/Tasks/PWGHF/taskBPlus.cxx
@@ -64,11 +64,18 @@ struct AddCollisionId {
 struct TaskBplus {
   HistogramRegistry registry{
     "registry",
-      {{"hmassD0", "2-prong candidates;inv. mass (#pi K) (GeV/#it{c}^{2});entries", {HistType::kTH1F, {{500, 0., 5.}}}},
-        {"hptcand", "B+ candidates;candidate #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
-        {"hptD0prong0", "2-prong candidates;prong0 #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
-        {"hptD0prong1", "2-prong candidates;prong1 #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
-        {"hEtaD0", "2-prong candidates;candidate #it{#eta};entries", {HistType::kTH1F, {{100, -2, 2.}}}}
+      {{"hmassD0", "2-prong candidates; inv. mass (#pi K) (GeV/#it{c}^{2});entries", {HistType::kTH1F, {{500, 0., 5.}}}},
+        {"hptD0prong0", "2-prong candidates; prong0 #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
+        {"hptD0prong1", "2-prong candidates; prong1 #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
+        {"hEtaD0", "2-prong candidates; candidate #it{#eta};entries", {HistType::kTH1F, {{100, -2, 2.}}}},
+        {"hptD0","D0 Candidates; #it{p}_{T} (GeV/#it{c});entries",{HistType::kTH1F, {{100, 0., 10.}}}},
+        {"hptBachTrk","pi track; #it{p}_{T} (GeV/#it{c});entries",{HistType::kTH1F, {{100, 0., 10.}}}},
+        {"hptD0inTrkLoop","D0 Cand(inside track loop); #it{p}_{T} (GeV/#it{c});entries",{HistType::kTH1F, {{100, 0., 10.}}}},
+        {"hptcand", "D0+pi candidates; #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
+        {"hpTBCand","B Candidates; #it{p}_{T} (GeV/#it{c});entries",{HistType::kTH1F, {{100, 0., 10.}}}},
+        {"hpTBprongD0","B prong D0; #it{p}_{T} (GeV/#it{c});entries",{HistType::kTH1F, {{100, 0., 10.}}}},
+        {"hpTBprongpi","B prong pi; #it{p}_{T} (GeV/#it{c});entries",{HistType::kTH1F, {{100, 0., 10.}}}},
+        {"hBCandInvmass","B candidates; Invmass (GeV/#it{c}^{2});entries", {HistType::kTH1F, {{1000, 0., 10.}}}}
       }
   };
 
@@ -120,7 +127,6 @@ struct TaskBplus {
       if (std::abs(candidate.eta()) > cutEtaCandMax) {
         continue;
       }
-
       if ((candidate.isSelD0bar() < d_selectionFlagD0bar) && (candidate.isSelD0() < d_selectionFlagD0))
         continue;
 
@@ -131,6 +137,7 @@ struct TaskBplus {
       registry.fill(HIST("hptD0prong0"), candidate.ptProng0());
       registry.fill(HIST("hptD0prong1"), candidate.ptProng1());
       registry.fill(HIST("hEtaD0"), candidate.eta());
+      registry.fill(HIST("hptD0"),candidate.pt());
 
       const std::array<float, 3> vertexD0 = {candidate.xSecondaryVertex(), candidate.ySecondaryVertex(), candidate.zSecondaryVertex()};
       const std::array<float, 3> momentumD0 = {candidate.px(), candidate.py(), candidate.pz()};
@@ -140,8 +147,8 @@ struct TaskBplus {
       auto prong0TrackParCov = getTrackParCov(prong0);
       auto prong1TrackParCov = getTrackParCov(prong1);
 
-      // LOGF(INFO, "All track: %d (prong0); %d (prong1)", candidate.index0().globalIndex(), candidate.index1().globalIndex());
-      // LOGF(INFO, "All track pT: %f (prong0); %f (prong1)", prong0.pt(), prong1.pt());
+      LOGF(INFO, "All track: %d (prong0); %d (prong1)", candidate.index0().globalIndex(), candidate.index1().globalIndex());
+      LOGF(INFO, "All track pT: %f (prong0); %f (prong1)", prong0.pt(), prong1.pt());
 
       // reconstruct D0 secondary vertex
       if (df.process(prong0TrackParCov, prong1TrackParCov) == 0) {
@@ -171,6 +178,8 @@ struct TaskBplus {
         if(candidate.index0Id() == track.globalIndex() || candidate.index1Id() == track.globalIndex())
           continue; //daughter track id and bachelor track id not the same
 
+        registry.fill(HIST("hptBachTrk"),track.pt());
+        registry.fill(HIST("hptD0inTrkLoop"),candidate.pt());
         registry.fill(HIST("hptcand"), candidate.pt() + track.pt());
 
         auto bachTrack = getTrackParCov(track);
@@ -184,16 +193,24 @@ struct TaskBplus {
         if(nCand == 0) continue;
 
         bfitter.propagateTracksToVertex();        // propagate the bachelor and D0 to the B+ vertex
-        bfitter.getTrack(0).getPxPyPzGlo(pvecD0); //momentum of D0 at the B+ vertex; Need to fill new D0+ pT
-        bfitter.getTrack(1).getPxPyPzGlo(pvecbach); //momentum of pi+ at the B+ vertex; Need to fill new p+ pT
+        bfitter.getTrack(0).getPxPyPzGlo(pvecD0); //momentum of D0 at the B+ vertex
+        bfitter.getTrack(1).getPxPyPzGlo(pvecbach); //momentum of pi+ at the B+ vertex
 
         pvecBCand = array{pvecbach[0] + pvecD0[0],
           pvecbach[1] + pvecD0[1],
           pvecbach[2] + pvecD0[2]};
 
-        // invariant mass
+        //B-candidate properties
         auto arrMom = array{pvecbach, pvecD0};
-        double massBP = RecoDecay::M(arrMom, array{massPi, massD0}); //Plot B+ mass
+        double massBP = RecoDecay::M(arrMom, array{massPi, massD0}); //B+ mass
+        double pTBP = RecoDecay::Pt(pvecBCand[0],pvecBCand[1]); //pT B+
+        double pTBprongD0 = RecoDecay::Pt(pvecD0[0], pvecD0[1]); //pT of B decay prong D0
+        double pTBprongpi = RecoDecay::Pt(pvecbach[0], pvecbach[1]); //pT of B decay prong pi
+
+        registry.fill(HIST("hBCandInvmass"), massBP);
+        registry.fill(HIST("hpTBCand"), pTBP);
+        registry.fill(HIST("hpTBprongD0"), pTBprongD0);
+        registry.fill(HIST("hpTBprongpi"), pTBprongpi);
 
       } //track loop
     }//D0 cand loop
